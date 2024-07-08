7f33ab9a1943c2ad9241368e21b44cd7b0258c83
Mamoon-Ahmad <mma282796@gmail.com>
2024-07-08 23:22:39 +0500

    New Branch

---
+++ src/Compound/NewBranch.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+function NewBranch(props) {
+  return (
+    <div>
+      asdrtyuio
+      <a href="">qwertyuiopgit</a>
+      <u>wertyuiop</u>
+    </div>
+  )
+}
+
+
+
+export default NewBranch
+
