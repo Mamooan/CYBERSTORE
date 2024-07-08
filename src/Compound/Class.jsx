b6a6d830d70d34484d6d0b38016c71243bf569cf
Mamoon-Ahmad <mma282796@gmail.com>
2024-07-08 15:07:03 +0500

    Reset

--- src/Compound/Class.jsx
+++ src/Compound/Class.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
 export default function Class() {
-  return (
-    <div>
-        git diff mean to what is add in you code
-      
-    </div>
-  )
+    return (
+        <div>
+            git diff mean to what is add in you code
+
+            git show id : is show that what change in our code
+
+            git blame file name
+
+            git reset --hard  id
+
+        </div>
+    )
 }
